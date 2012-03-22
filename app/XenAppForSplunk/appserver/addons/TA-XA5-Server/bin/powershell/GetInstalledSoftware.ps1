3dc866e31f1e2f6a934987bbd322a7a4bdd2c0ff
Brandon Shell <tshell@gmail.com>
2012-03-21 21:09:12 -0400

    Fixed bug in GetInstalledSoftware.ps1

--- app/XenAppForSplunk/appserver/addons/TA-XA5-Server/bin/powershell/GetInstalledSoftware.ps1
+++ app/XenAppForSplunk/appserver/addons/TA-XA5-Server/bin/powershell/GetInstalledSoftware.ps1
@@ -4,43 +4,50 @@ $ScriptRunTime = (get-date).ToFileTime()
   
 $RemoteRegistry = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey("LocalMachine",$Computer)   
 
-$RegKey = $RemoteRegistry.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\")  
-foreach($key in $RegKey.GetSubKeyNames())   
-{   
-    $SubKey = $RegKey.OpenSubKey($key)
-    $DisplayName = $SubKey.GetValue("DisplayName")
-    if($DisplayName)
+$RegKey = $RemoteRegistry.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\")
+if($RegKey)
+{
+    foreach($key in $RegKey.GetSubKeyNames())   
     {
-        $output  = 'Name="{0}"'            -f $DisplayName
-        $output += 'Version="{0}" '        -f ($SubKey.GetValue("DisplayVersion"))  
-        $output += 'InstallLocation="{0}"' -f ($SubKey.GetValue("InstallLocation")) 
-        $output += 'Vendor="{0}"'          -f ($SubKey.GetValue("Publisher")) 
-        $output += 'ScriptRunTime="{0}"'   -f $ScriptRunTime
-    }
-    
-    if($output)
-    {
-        Write-Host ("{0:MM/dd/yyyy HH:mm:ss} GMT - {1}" -f ((get-date).ToUniversalTime()),( $output -join " " ))
-    }
-}   
+        $output = @()
+        $SubKey = $RegKey.OpenSubKey($key)
+        $DisplayName = $SubKey.GetValue("DisplayName")
+        if($DisplayName)
+        {
+            $output += 'Name="{0}"'            -f $DisplayName
+            $output += 'Version="{0}"'         -f ($SubKey.GetValue("DisplayVersion"))  
+            $output += 'InstallLocation="{0}"' -f ($SubKey.GetValue("InstallLocation")) 
+            $output += 'Vendor="{0}"'          -f ($SubKey.GetValue("Publisher")) 
+            $output += 'ScriptRunTime="{0}"'   -f $ScriptRunTime
+        }
+        
+        if($output.count -gt 0)
+        {
+            Write-Host ("{0:MM/dd/yyyy HH:mm:ss} GMT - {1}" -f ((get-date).ToUniversalTime()),( $output -join " " ))
+        }
+    }   
+}
 
-$RegKey = $RemoteRegistry.OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall")   
-foreach($key in $RegKey.GetSubKeyNames())   
-{   
-    $SubKey = $RegKey.OpenSubKey($key)   
-    $DisplayName = $SubKey.GetValue("DisplayName")
-    if($DisplayName)
+$RegKey = $RemoteRegistry.OpenSubKey("SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall")
+if($RegKey)
+{
+    foreach($key in $RegKey.GetSubKeyNames())   
     {
-        $output  = 'Name="{0}" '           -f $DisplayName
-        $output += 'Version="{0}" '        -f ($SubKey.GetValue("DisplayVersion")) 
-        $output += 'InstallLocation="{0}"' -f ($SubKey.GetValue("InstallLocation"))
-        $output += 'Vendor="{0}"'          -f ($SubKey.GetValue("Publisher"))
-        $output += 'ScriptRunTime="{0}"'   -f $ScriptRunTime
-    }
-    
-    if($output)
-    {
-        Write-Host ("{0:MM/dd/yyyy HH:mm:ss} GMT - {1}" -f ((get-date).ToUniversalTime()),( $output -join " " ))
-    }
-}   
-
+        $output = @()
+        $SubKey = $RegKey.OpenSubKey($key)   
+        $DisplayName = $SubKey.GetValue("DisplayName")
+        if($DisplayName)
+        {
+            $output += 'Name="{0}"'            -f $DisplayName
+            $output += 'Version="{0}"'         -f ($SubKey.GetValue("DisplayVersion")) 
+            $output += 'InstallLocation="{0}"' -f ($SubKey.GetValue("InstallLocation"))
+            $output += 'Vendor="{0}"'          -f ($SubKey.GetValue("Publisher"))
+            $output += 'ScriptRunTime="{0}"'   -f $ScriptRunTime
+        }
+        
+        if($output.count -gt 0)
+        {
+            Write-Host ("{0:MM/dd/yyyy HH:mm:ss} GMT - {1}" -f ((get-date).ToUniversalTime()),( $output -join " " ))
+        }
+    }   
+}
